3a149a34f1dbee2d9f799cec208c50e0ec987d4c
LeonardoSaraceno <leonardosaraceno03@gmail.com>
2022-10-24 17:38:19 +0200

    correzione ordine variabili

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,13 @@
 
-var element = document.getElementById("sendDatas");
+var element = document.getElementById('sendDatas')
 element.addEventListener('click',
 function sendDatas(){
+    let emailServer = ["ciao", "leo", "saluti"];
+    let emailValue = document.getElementById('inputEmail')
     for(let i=0; i<emailServer.length; i++){
 
     
-    let emailServer = ["ciao", "leo", "saluti"];
-    let emailValue = document.getElementById('inputEmail')
+    
     if(emailServer[i] == emailValue){
         console.log(`${emailValue} is already in the Database`)
     }
